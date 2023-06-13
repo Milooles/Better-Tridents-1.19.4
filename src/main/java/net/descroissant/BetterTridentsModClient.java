683eb49d494bafeccd80f04f1cd2016a68152b01
Milooles <doodoododo1701@gmail.com>
2023-06-14 09:00:57 +1000

    Base Template Upload

---
+++ src/main/java/net/descroissant/BetterTridentsModClient.java
@@ -0,0 +1,11 @@
+package net.descroissant;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterTridentsModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
